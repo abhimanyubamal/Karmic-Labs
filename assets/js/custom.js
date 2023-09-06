5f13a4392cf8d0b1cd0064cc473d01cc80e3ce6a
abamal <abamal@uoguelph.ca>
2023-09-06 12:51:31 +0530

    First terminal commit

---
+++ assets/js/custom.js
@@ -0,0 +1,31 @@
+let label = document.querySelector("label");
+
+
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1200,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+    ]
+});
