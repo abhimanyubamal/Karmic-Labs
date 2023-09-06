ce84b09400e3de5155fc73417e79bf94e2e96501
abamal <abamal@uoguelph.ca>
2023-09-06 14:39:23 +0530

    Responsive nav-bar completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,22 @@
-let label = document.querySelector("label");
+let label = document.querySelector("#label");
+let menu = document.querySelector(".menu");
+let btnWrapper = document.querySelector(".button-wrapper");
 
+let flag = 1;
+
+label.addEventListener("click", () => {
+    // menu.classList.toggle("d-flex");
+    if (flag === 1) {
+        menu.style.top = 0;
+        btnWrapper.style.top = 0;
+        flag--;
+    }
+    else {
+        menu.style.top = "-100%";
+        btnWrapper.style.top = "-100%";
+        flag++;
+    }
+});
 
 $('.slider').slick({
     dots: false,
