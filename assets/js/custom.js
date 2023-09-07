fa5729408566b491e226ae792793d2bfd733a14d
abamal <abamal@uoguelph.ca>
2023-09-07 16:04:27 +0530

    Anchor links added, spacing issue resolved, background-size: 100% 100%; implemented

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,6 +8,10 @@ aTags.forEach((one) => {
     one.addEventListener("click", () => {
         console.log(one);
         if (flag === 0) { 
+            document.body.classList.remove("overflow-y-hidden");
+            hiddenButton.classList.add("d-none");
+            menu.style.top = "-100%";
+            flag++;
         }
     })
 } )
@@ -19,11 +23,13 @@ label.addEventListener("click", () => {
     if (flag === 1) {
         menu.style.top = 0;
         hiddenButton.classList.remove("d-none");
+        document.body.classList.add("overflow-y-hidden");
         flag--;
     }
     else {
         menu.style.top = "-100%";
         hiddenButton.classList.add("d-none");
+        document.body.classList.remove("overflow-y-hidden");
         flag++;
     }
 });
